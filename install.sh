a476b3207a9095af904c0ab9329144a5d51cff92
hitu-1995 <110018373+hitu-1995@users.noreply.github.com>
2025-04-19 15:22:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://codepipeline-us-east-1-175071939152/tomcat/ROOT.war /tmp
+aws s3 cp s3://elasticbeanstalk-ap-south-1-175071939152/tomcat/ROOT.war /tmp
 sudo mv /tmp/ROOT.war /usr/share/tomcat/webapps/ROOT.war
 sudo service tomcat restart
